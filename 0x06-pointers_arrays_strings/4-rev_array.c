37535a20de85bbef4048d1cf2c523da1cc2e2984
ibrnad <57199774+ibranad@users.noreply.github.com>
2023-03-30 19:53:43 +0000

    last exercice

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * reverse_array - Entry Function
+ * @a: int
+ * @n: int
+ */
+void reverse_array(int *a, int n)
+{
+	int i, swap;
+
+	for (i = 0 ; i < n / 2 ; i++)
+	{
+		swap = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = swap;
+	}
+}
